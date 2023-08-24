dde8685a3972a7a566ccd2fa7293ab24b1510fc6
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-08-23 21:43:05 -0700

    Update Bingo-or-not.js

--- Bingo-or-not.js
+++ Bingo-or-not.js
@@ -11,3 +11,11 @@ Your outputs should be "WIN" or "LOSE" respectively.
 
 
 // Solution
+
+function bingo(a) {
+  let n = [2,9,14,7,15]
+  for(let i =0; i< n.length;i++){
+  if(!a.includes(n[i])){ return 'LOSE' }
+  }
+  return 'WIN'
+}
